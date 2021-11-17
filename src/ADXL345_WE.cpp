0bd3f79d85397d530c71be1de32656e16d6ac5c7
Ivols84 <94567652+Ivols84@users.noreply.github.com>
2021-11-17 21:14:53 +0100

    Update ADXL345_WE.cpp
    I think the pitch equation is wrong.

--- src/ADXL345_WE.cpp
+++ src/ADXL345_WE.cpp
@@ -322,7 +322,7 @@ String ADXL345_WE::getOrientationAsString(){
 
 float ADXL345_WE::getPitch(){
     getGValues();
-    float pitch = (atan2(gVal.x, sqrt(gVal.x*gVal.y + gVal.z*gVal.z))*180.0)/M_PI;
+    float pitch = (atan2(gVal.x, sqrt(gVal.y*gVal.y + gVal.z*gVal.z))*180.0)/M_PI;
     return pitch;
 }
     
